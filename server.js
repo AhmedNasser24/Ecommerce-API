a647a4b8d94ec16e1eabf5131b7104e9b3194a92
AhmedNasser24 <eng.ahmednasser2024@gmail.com>
2026-05-06 16:07:42 +0300

    init

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config({ path: "config.env" });
+const app = express();
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+  console.log("You are welcome to my api");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
