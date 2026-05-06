5a691ab27dd4c2cabb8e6c7082de4479c51c1632
AhmedNasser24 <eng.ahmednasser2024@gmail.com>
2026-05-06 22:09:08 +0300

    connect to server

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require("express");
-const dotenv = require("dotenv");
-dotenv.config({ path: "config.env" });
+const dotenv = require("dotenv").config();
+const morgan = require("morgan");
+const mongoose = require("mongoose")
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((error) => {
+  console.log(error);
+  process.exit(1);
+})
+
 const app = express();
+
+if(process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 
 app.get("/", (req, res) => {
