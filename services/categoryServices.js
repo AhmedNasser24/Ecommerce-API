e45ef8474e6a1812a62374385226326330dc5421
AhmedNasser24 <eng.ahmednasser2024@gmail.com>
2026-05-07 18:35:10 +0300

    make pagination for category

--- services/categoryServices.js
+++ services/categoryServices.js
@@ -13,8 +13,11 @@ exports.createCategory = asyncHandler(async (req, res) => {
 });
 
 exports.getCategories = asyncHandler(async (req, res) => {
-  const categories = await CategoryModel.find();
-  res.status(200).json(categories);
+  const page = req.query.page * 1 || 1;
+  const limit = req.query.limit * 1 || 20;
+  const skip = (page - 1) * limit;
+  const categories = await CategoryModel.find().skip(skip).limit(limit);
+  res.status(200).json({ results: categories.length, page, data: categories });
 });
 
 exports.getCategory = asyncHandler(async (req, res) => {
@@ -35,8 +38,8 @@ exports.updateCategory = asyncHandler(async (req, res) => {
   const { name } = req.body;
   const category = await CategoryModel.findByIdAndUpdate(
     { _id: id },
-    { name, slug: slugify(name) },~
-    { new: true },
+    { name, slug: slugify(name) },
+    ~{ new: true },
   );
   if (!category) {
     res.status(404).json({ message: "Category not found" });
