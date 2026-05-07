3e99a5b4ba4bbe0c24633b95383bc96d95fad1d2
AhmedNasser24 <eng.ahmednasser2024@gmail.com>
2026-05-07 23:18:09 +0300

    handle error for wrong and valid id

--- services/categoryServices.js
+++ services/categoryServices.js
@@ -1,4 +1,5 @@
 const CategoryModel = require("../models/categoryModel");
+const ApiError = require("../utils/ApiError");
 const slugify = require("slugify");
 const asyncHandler = require("express-async-handler");
 exports.createCategory = asyncHandler(async (req, res) => {
@@ -21,11 +22,11 @@ exports.getCategories = asyncHandler(async (req, res) => {
   res.status(200).json({ results: categories.length, page, data: categories });
 });
 
-exports.getCategory = asyncHandler(async (req, res) => {
+exports.getCategory = asyncHandler(async (req, res , next) => {
   const { id } = req.params;
   const category = await CategoryModel.findById(id);
   if (!category) {
-    res.status(404).json({ message: "Category not found" });
+    return next(new ApiError(`Category with ID ${id} not found`, 404));
   }
   res.status(200).json(category);
 });
@@ -34,16 +35,16 @@ exports.getCategory = asyncHandler(async (req, res) => {
 // @route   PUT /api/categories/:id
 // @access  Private
 
-exports.updateCategory = asyncHandler(async (req, res) => {
+exports.updateCategory = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
   const { name } = req.body;
   const category = await CategoryModel.findByIdAndUpdate(
     { _id: id },
     { name, slug: slugify(name) },
-    ~{ new: true },
+    { new: true },
   );
   if (!category) {
-    res.status(404).json({ message: "Category not found" });
+    return next(new ApiError(`Category with ID ${id} not found`, 404));
   }
   res.status(200).json(category);
 });
@@ -51,11 +52,11 @@ exports.updateCategory = asyncHandler(async (req, res) => {
 // @desc    Delete category by ID
 // @route   DELETE /api/categories/:id
 // @access  Private
-exports.deleteCategory = asyncHandler(async (req, res) => {
+exports.deleteCategory = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
   const category = await CategoryModel.findByIdAndDelete(id);
   if (!category) {
-    res.status(404).json({ message: "Category not found" });
+    return next(new ApiError(`Category with ID ${id} not found`, 404));
   }
-  res.status(200).send;
+  res.status(204).send();
 });
