bf014f8882aa0a7c320816988f5f2160cc2cbed5
AhmedNasser24 <eng.ahmednasser2024@gmail.com>
2026-05-07 18:25:39 +0300

    make crud services

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,9 +1,14 @@
-
 const express = require("express");
 
 const router = express.Router();
 
-const {getCategories,createCategory} = require("../services/categoryServices");
+const {
+  getCategories,
+  createCategory,
+  getCategory,
+  updateCategory,
+  deleteCategory, 
+} = require("../services/categoryServices");
 
 // @route   GET /categories
 // @desc    Get all categories
@@ -13,9 +18,7 @@ router.get("/", getCategories);
 // @route   GET /categories/:id
 // @desc    Get a single category by ID
 // @access  Public
-router.get("/:id", (req, res) => {
-  res.send(`Get category ${req.params.id}`);
-});
+router.get("/:id", getCategory);
 
 // @route   POST /categories
 // @desc    Create a new category
@@ -25,15 +28,11 @@ router.post("/", createCategory);
 // @route   PUT /categories/:id
 // @desc    Update a category by ID
 // @access  Public
-router.put("/:id", (req, res) => {
-  res.send(`Update category ${req.params.id}`);
-});
+router.put("/:id", updateCategory);
 
 // @route   DELETE /categories/:id
 // @desc    Delete a category by ID
 // @access  Public
-router.delete("/:id", (req, res) => {
-  res.send(`Delete category ${req.params.id}`);
-});
+router.delete("/:id", deleteCategory);
 
 module.exports = router;
--- services/categoryServices.js
+++ services/categoryServices.js
@@ -2,19 +2,56 @@ const CategoryModel = require("../models/categoryModel");
 const slugify = require("slugify");
 const asyncHandler = require("express-async-handler");
 exports.createCategory = asyncHandler(async (req, res) => {
-    const { name } = req.body; // نستخدم destructing لجلب الاسم
-    const newCategory = new CategoryModel({ name, slug: slugify(name) });
+  const { name } = req.body; // نستخدم destructing لجلب الاسم
+  const newCategory = new CategoryModel({ name, slug: slugify(name) });
 
-    // نستخدم await لنضمن أن البرنامج "ينتظر" الحفظ قبل الانتقال للسطر التالي
-    const savedCategory = await newCategory.save();
+  // نستخدم await لنضمن أن البرنامج "ينتظر" الحفظ قبل الانتقال للسطر التالي
+  const savedCategory = await newCategory.save();
 
-    console.log("Category Saved:", savedCategory); // لرؤيتها في Terminal الماك ميني
-    res.status(201).json(savedCategory); // نرسل البيانات التي تم حفظها فعلياً
- 
+  console.log("Category Saved:", savedCategory); // لرؤيتها في Terminal الماك ميني
+  res.status(201).json(savedCategory); // نرسل البيانات التي تم حفظها فعلياً
 });
-
 
 exports.getCategories = asyncHandler(async (req, res) => {
   const categories = await CategoryModel.find();
   res.status(200).json(categories);
+});
+
+exports.getCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const category = await CategoryModel.findById(id);
+  if (!category) {
+    res.status(404).json({ message: "Category not found" });
+  }
+  res.status(200).json(category);
+});
+
+// @desc    Update category by ID
+// @route   PUT /api/categories/:id
+// @access  Private
+
+exports.updateCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const category = await CategoryModel.findByIdAndUpdate(
+    { _id: id },
+    { name, slug: slugify(name) },~
+    { new: true },
+  );
+  if (!category) {
+    res.status(404).json({ message: "Category not found" });
+  }
+  res.status(200).json(category);
+});
+
+// @desc    Delete category by ID
+// @route   DELETE /api/categories/:id
+// @access  Private
+exports.deleteCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const category = await CategoryModel.findByIdAndDelete(id);
+  if (!category) {
+    res.status(404).json({ message: "Category not found" });
+  }
+  res.status(200).send;
 });
