e3c33d7fd054894892fa50038e02ea22e60f7c67
AhmedNasser24 <eng.ahmednasser2024@gmail.com>
2026-05-10 19:15:41 +0300

    make product model

---
+++ features/product/models/productModels.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    unique: [true, "Product title must be unique"],
+    required: [true, "Product title is required"],
+    minlength: [2, "Product title must be at least 2 characters"],
+    maxlength: [32, "Product title must be at most 32 characters"],
+  },
+  slug: {
+    type: String,
+    unique: [true, "Product slug must be unique"],
+    required: [true, "Product slug is required"],
+    minlength: [2, "Product slug must be at least 2 characters"],
+    maxlength: [32, "Product slug must be at most 32 characters"],
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: [true, "Product must belong to a category"],
+  },
+  subCategory: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "SubCategory",
+    required: [true, "Product must belong to a subcategory"],
+  },
+  brand: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Brand",
+  },
+  price: {
+    type: Number,
+    required: [true, "Product price is required"],
+    min: [0, "Product price must be at least 0"],
+  },
+  priceAfterDiscount : {
+    type: Number,
+    min: [0, "Product price must be at least 0"],
+  }
+});
+
+const ProductModel = mongoose.model("Product", ProductSchema);
+
+module.exports = ProductModel;
