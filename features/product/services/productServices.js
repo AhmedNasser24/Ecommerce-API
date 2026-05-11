eefda07bfffb267061b12f7e932362cad8e8dbad
AhmedNasser24 <eng.ahmednasser2024@gmail.com>
2026-05-11 14:29:29 +0300

    make filteration and sorting for product

--- features/product/services/productServices.js
+++ features/product/services/productServices.js
@@ -4,16 +4,48 @@ const ApiError = require("../../../utils/ApiError");
 const slugify = require("slugify");
 
 const getAllProducts = asyncHandler(async (req, res) => {
+  //pagination
   const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit) || 20;
   const skip = (page - 1) * limit;
-  const products = await ProductModel.find().skip(skip).limit(limit).populate("category").populate("subcategory");
+
+  // filteration
+  const queryObj = { ...req.query };
+  const excludedFields = ["page", "limit", "sort", "fields"];
+  excludedFields.forEach((field) => delete queryObj[field]);
+
+  // Apply filtration using [gte, gt, lte, lt]
+  let queryStr = JSON.stringify(queryObj);
+  console.log(queryStr);
+  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+  console.log(queryStr);
+
+  // 2) Sorting
+  let sortBy;
+  if (req.query.sort) {
+    // @ts-ignore
+    sortBy = req.query.sort.split(",").join(" ");
+  } else {
+    sortBy = "-createdAt";
+  }
+
+  // build mongoose query
+  const mongooseQuery = ProductModel.find(JSON.parse(queryStr))
+    .skip(skip)
+    .sort(sortBy)
+    .limit(limit)
+    .populate("category")
+    .populate("subcategory");
+
+  //get all products
+  const products = await mongooseQuery;
+
   res.status(200).json(products);
 });
 
-const createProduct = asyncHandler(async (req, res,next) => {
+const createProduct = asyncHandler(async (req, res, next) => {
   req.body.slug = slugify(req.body.title);
-  if(!req.body.priceAfterDiscount){
+  if (!req.body.priceAfterDiscount) {
     req.body.priceAfterDiscount = req.body.price;
   }
   const product = await ProductModel.create(req.body);
@@ -23,23 +55,31 @@ const createProduct = asyncHandler(async (req, res,next) => {
   res.status(201).json(product);
 });
 
-const getProduct = asyncHandler(async (req, res,next) => {
-  const product = await ProductModel.findById(req.params.id).populate("category").populate("subcategory");
-  if(!product){
+const getProduct = asyncHandler(async (req, res, next) => {
+  const product = await ProductModel.findById(req.params.id)
+    .populate("category")
+    .populate("subcategory");
+  if (!product) {
     return next(new ApiError("Product not found", 404));
   }
   res.status(200).json(product);
 });
-const updateProduct = asyncHandler(async (req, res,next) => {
-  const product = await ProductModel.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true }).populate("category").populate("subcategory");
-  if(!product){
+const updateProduct = asyncHandler(async (req, res, next) => {
+  const product = await ProductModel.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true, runValidators: true },
+  )
+    .populate("category")
+    .populate("subcategory");
+  if (!product) {
     return next(new ApiError("Product not found", 404));
   }
   res.status(200).json(product);
 });
-const deleteProduct = asyncHandler(async (req, res,next) => {
+const deleteProduct = asyncHandler(async (req, res, next) => {
   const product = await ProductModel.findByIdAndDelete(req.params.id);
-  if(!product){
+  if (!product) {
     return next(new ApiError("Product not found", 404));
   }
   res.status(204).send();
