d45ff5abe55013fceac189a4c1d54d2c0b3a162f
AhmedNasser24 <eng.ahmednasser2024@gmail.com>
2026-05-19 19:10:40 +0300

    make user schema

---
+++ features/user/models/userModels.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 32,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 6,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    phone: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 8,
+    },
+    image: {
+      type: String,
+      trim: true,
+    },
+    address: {
+      type: String,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const UserModel = mongoose.model("User", userSchema);
+module.exports = UserModel;
